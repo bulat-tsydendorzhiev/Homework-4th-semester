21875f91fa9bcb0a7b97d4752c8eb74d26f58769
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-10 01:38:13 +0300

    feat: add counting functions.

---
+++ Homeworks/Homework2/EvenNumbers/EvenNumbers/EvenNumbers.fs
@@ -0,0 +1,11 @@
+﻿namespace EvenNumbers
+
+module EvenNumbersCounter =
+    let countViaMap list = 
+        list |> Seq.map(fun x -> (x + 1) % 2) |> Seq.sum
+
+    let countViaFilter list =
+        list |> Seq.filter(fun x -> x % 2 = 0) |> Seq.length
+
+    let countViaFold list =
+        list |> Seq.fold(fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
