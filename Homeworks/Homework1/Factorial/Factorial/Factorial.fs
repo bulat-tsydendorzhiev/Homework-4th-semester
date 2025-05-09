90314d4603cedb0b45b66a3fd2409c003d559456
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-09 22:59:07 +0300

    feat: add factorial function.

---
+++ Homeworks/Homework1/Factorial/Factorial/Factorial.fs
@@ -0,0 +1,10 @@
+﻿module Factorial
+
+let factorial number =
+    let rec factorialRecursive acc i =
+        match i with
+        | 0 -> Some(acc)
+        | n -> factorialRecursive (acc * n) (i - 1)
+
+    if number < 0 then None
+    else factorialRecursive 1 number
