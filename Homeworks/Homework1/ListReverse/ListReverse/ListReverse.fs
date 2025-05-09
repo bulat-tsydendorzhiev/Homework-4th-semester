8fb1d61157858e4bf714590cbf035fa73e71ec66
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-09 23:50:52 +0300

    feat: add list reverse function.

---
+++ Homeworks/Homework1/ListReverse/ListReverse/ListReverse.fs
@@ -0,0 +1,8 @@
+﻿module ListReverse
+
+let reverseList list =
+    let rec reverseListRecursive ls acc =
+        match ls with
+        | [] -> acc
+        | head :: tail -> reverseListRecursive tail (head :: acc)
+    reverseListRecursive list []
