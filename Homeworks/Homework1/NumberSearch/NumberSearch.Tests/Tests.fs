553eb90a19f6a8656b96fe6f5fa6a9da58fc26f2
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-10 00:26:30 +0300

    test: add tests.

---
+++ Homeworks/Homework1/NumberSearch/NumberSearch.Tests/Tests.fs
@@ -0,0 +1,26 @@
+﻿module NumberSearch.Tests
+
+open NUnit.Framework
+open NumberSearch
+
+let notContainedNumberTestCases = 
+    [
+        ([1; 2; 3], 4);
+        ([], 1);
+        ([1], 2);
+    ] |> List.map(fun (list, notContainedNumber) -> TestCaseData(list, notContainedNumber))
+
+let containedNumberTestCases = 
+    [
+        ([1; 2; 3], 1, 0);
+        ([4; 5; 6], 6, 2);
+        ([1], 1, 0);
+    ] |> List.map(fun (list, containedNumber, position) -> TestCaseData(list, containedNumber, position))
+
+[<TestCaseSource("notContainedNumberTestCases")>]
+let ``Search should return None with not contained number`` list notContainedNumber =
+    Assert.That(searchNumber list notContainedNumber = None)
+
+[<TestCaseSource("containedNumberTestCases")>]
+let ``Search should return correct position of contained number`` list containedNumber position =
+    Assert.That(searchNumber list containedNumber = Some(position))
