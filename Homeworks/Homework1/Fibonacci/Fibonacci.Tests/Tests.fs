9fc1d3821210b51fbd4157133b26bef71641116c
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-09 23:38:31 +0300

    test: add tests.

---
+++ Homeworks/Homework1/Fibonacci/Fibonacci.Tests/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Fibonacci.Tests
+
+open NUnit.Framework
+open Fibonacci
+
+[<Test>]
+let ``N-th Fibonacci number should be None with invalid N`` () =
+    Assert.That(fibonacci -1 = None)
+
+[<TestCase(0, 0)>]
+[<TestCase(1, 1)>]
+[<TestCase(2, 1)>]
+[<TestCase(3, 2)>]
+let ``N-th Fibonacci number should be expected with valid N`` n expected =
+    Assert.That(fibonacci n = Some(expected))
