f29c9fc4f8aaf580f53adff6dbbada3b5b3ec4a8
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-09 23:28:17 +0300

    feat: add n-th fibonacci number computing function.

---
+++ Homeworks/Homework1/Fibonacci/Fibonacci/Fibonacci.fs
@@ -0,0 +1,10 @@
+﻿module Fibonacci
+
+let fibonacci n =
+    let rec fibonacciRecursive current next i =
+        match i with
+        | 0 -> Some(current)
+        | _ -> fibonacciRecursive next (current + next) (i - 1)
+
+    if n < 0 then None
+    else fibonacciRecursive 0 1 n
