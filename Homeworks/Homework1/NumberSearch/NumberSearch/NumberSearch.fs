ee02494c6be8cfa3f948306419f4b5b4f54265c7
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-05-10 00:12:32 +0300

    feat: add number searching function.

---
+++ Homeworks/Homework1/NumberSearch/NumberSearch/NumberSearch.fs
@@ -0,0 +1,11 @@
+﻿module NumberSearch
+
+let searchNumber list number =
+    let rec searchNumberInternal ls i =
+        match ls with
+        | [] -> None
+        | head :: tail -> 
+            if head = number then Some(i)
+            else searchNumberInternal tail (i + 1)
+
+    searchNumberInternal list 0
