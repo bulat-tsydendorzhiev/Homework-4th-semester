165651feb55b765a54d25caefaa7bf340fa7cb80
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-01 14:42:28 +0300

    fix: isPrime work correctly; test: add tests.

---
+++ Homeworks/Homework2/PrimeNumbers/PrimeNumbers.Tests/Tests.fs
@@ -0,0 +1,16 @@
+﻿module PrimeNumbers.Tests
+
+open NUnit.Framework
+open FsUnit
+open PrimeNumbers
+
+let testData = 
+    [
+        (1, seq{2});
+        (5, seq{2; 3; 5; 7; 11});
+        (10, seq{2; 3; 5; 7; 11; 13; 17; 19; 23; 29});
+    ] |> List.map (fun (n, expected) -> TestCaseData(n, expected))
+
+[<TestCaseSource("testData")>]
+let ``apt`` n expected =
+    generatePrimeNumbers |> Seq.take n |> should equal expected
