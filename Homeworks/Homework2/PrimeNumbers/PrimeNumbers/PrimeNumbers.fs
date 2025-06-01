f36f683d0259fa40ba33e829c89d7c6cd4c52a11
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-01 14:30:47 +0300

    feat: add generator of prime numbers.

---
+++ Homeworks/Homework2/PrimeNumbers/PrimeNumbers/PrimeNumbers.fs
@@ -0,0 +1,7 @@
+﻿module PrimeNumbers
+
+let isPrime number = 
+    Seq.exists (fun d -> number % d = 0) {2..int(sqrt(float(number)))}
+
+let generatePrimeNumbers = 
+    Seq.initInfinite(fun x -> x + 2) |> Seq.filter isPrime
