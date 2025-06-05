63d615d95f711c56c2ab9617a89d943c7559e33b
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-05 14:58:04 +0300

    feat: add "Rhomb" solution.

---
+++ Tests/Rewrite/Rewrite/Rhomb.fs
@@ -0,0 +1,12 @@
+﻿module Rhomb
+
+/// Creates a rhomb of `*` characters with side `n`
+let createRhombLines n =
+    let spaceCount k = abs (n - 1 - k)
+    let starCount k = 2 * (n - spaceCount k) - 1
+    [0..2 * (n - 1)] |> List.map (fun k -> 
+        String.replicate (spaceCount k) " " + String.replicate (starCount k) "*" + String.replicate (spaceCount k) " ")
+
+let printRhomb n =
+    createRhombLines n
+    |> List.iter (printfn "%s")
