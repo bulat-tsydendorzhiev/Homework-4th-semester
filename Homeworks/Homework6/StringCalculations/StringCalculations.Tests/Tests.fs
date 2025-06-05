b564409d21a884a6359163c90deb6380f4d55b9b
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-05 21:31:17 +0300

    test: add tests; feat: remove unnecessary matching.

---
+++ Homeworks/Homework6/StringCalculations/StringCalculations.Tests/Tests.fs
@@ -0,0 +1,25 @@
+﻿module StringCalculations.Tests
+
+open NUnit.Framework
+open FsUnit
+open StringCalculations
+
+[<Test>]
+let ``calculate stop and return None with invalid integer`` () =
+    let result = calculate {
+        let! a = "5"
+        let! b = "minus seven"
+        return a + b
+    }
+
+    result |> should equal None
+
+[<Test>]
+let ``calculate should work with valid integers`` () =
+    let result = calculate {
+        let! a = "5"
+        let! b = "-7"
+        return a + b
+    }
+
+    result |> should equal (Some -2)
--- Homeworks/Homework6/StringCalculations/StringCalculations/StringCalculationsBuilder.fs
+++ Homeworks/Homework6/StringCalculations/StringCalculations/StringCalculationsBuilder.fs
@@ -1,14 +1,12 @@
 ﻿module StringCalculations
 
 type StringCalculationsBuilder () =
-    member _.Bind(str: string, func: int -> option<int>) = 
+    member _.Bind(str : string, func : int -> option<int>) = 
         match System.Int32.TryParse str with
         | true, int -> int |> func
         | _ -> None
 
-    member _.Return(str: string) = 
-        match System.Int32.TryParse str with
-        | true, int -> Some int
-        | _ -> None
+    member _.Return(value : int) = 
+        Some(value)
 
-let calculate = StringCalculationsBuilder
+let calculate = StringCalculationsBuilder ()
