17b778645f008fd696cd7e1b43ea21ba5a17d60c
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-05 21:03:13 +0300

    test: add tests; fix: add rounding to Return member.

---
+++ Homeworks/Homework6/RoundingWorkflow/RoundingWorkflow.Tests/Tests.fs
@@ -0,0 +1,45 @@
+﻿module RoundingWorkflow.Tests
+
+open NUnit.Framework
+open FsUnit
+open RoundingWorkflow
+
+[<Test>]
+let ``Result should be rounded`` () =
+    let result = rounding 3 {
+        return 0.167 / 3.5
+    }
+
+    result |> should equal 0.048
+
+[<Test>]
+let ``Rounding with a zero decimal`` () =
+    let result = rounding 0 {
+        let! a = 160.123 / 4.123
+        let! b = 3.5
+        return a / b
+    }
+
+    result |> should equal 10.0
+
+[<Test>]
+let ``Rounding negative numbers`` () =
+    let result = rounding 3 {
+        let! a = -125.2 / 7.1
+        return a
+    }
+
+    result |> should equal -17.634
+
+[<Test>]
+let ``Rounding with complex calculation`` () =
+    let result = rounding 4 {
+        let! r = 1.2345
+        let! pi = System.Math.PI
+
+        let! s = pi * r * r
+
+        return s
+    }
+
+    result |> should equal 4.7878
--- Homeworks/Homework6/RoundingWorkflow/RoundingWorkflow/RoundingBuilder.fs
+++ Homeworks/Homework6/RoundingWorkflow/RoundingWorkflow/RoundingBuilder.fs
@@ -6,4 +6,6 @@ type RoundingWorkflowBuilder (decimal : int) =
     member _.Bind(value: float, func: float -> float) =
         Math.Round (value, decimal) |> func
 
-    member _.Return(value) = value
+    member _.Return(value: float) = Math.Round (value, decimal)
+
+let rounding decimal = RoundingWorkflowBuilder (decimal)
