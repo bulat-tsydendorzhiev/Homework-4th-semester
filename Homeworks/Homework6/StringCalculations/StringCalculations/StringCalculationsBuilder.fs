f6e947458f0968269cb836dd81b02b1da4a899b7
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-05 21:15:35 +0300

    feat: add builder for string calculations.

---
+++ Homeworks/Homework6/StringCalculations/StringCalculations/StringCalculationsBuilder.fs
@@ -0,0 +1,14 @@
+﻿module StringCalculations
+
+type StringCalculationsBuilder () =
+    member _.Bind(str: string, func: int -> option<int>) = 
+        match System.Int32.TryParse str with
+        | true, int -> int |> func
+        | _ -> None
+
+    member _.Return(str: string) = 
+        match System.Int32.TryParse str with
+        | true, int -> Some int
+        | _ -> None
+
+let calculate = StringCalculationsBuilder
