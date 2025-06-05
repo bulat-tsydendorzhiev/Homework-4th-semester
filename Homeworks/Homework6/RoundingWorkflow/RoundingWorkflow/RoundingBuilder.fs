43b390a28be34d4b37f8c55d1cd5f1d49ff11d3c
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2025-06-05 20:29:33 +0300

    feat: add rounding workflow builder.

---
+++ Homeworks/Homework6/RoundingWorkflow/RoundingWorkflow/RoundingBuilder.fs
@@ -0,0 +1,9 @@
+﻿module RoundingWorkflow
+
+open System
+
+type RoundingWorkflowBuilder (decimal : int) =
+    member _.Bind(value: float, func: float -> float) =
+        Math.Round (value, decimal) |> func
+
+    member _.Return(value) = value
